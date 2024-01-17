8b73b5a7f4ec5d7a08b24cb3785e3d8162f10b02
Rafael Rocha <rocharafaelh@gmail.com>
2024-01-16 21:04:35 -0300

    backgrou task, with status check and get result

--- models/data_models.py
+++ models/data_models.py
@@ -3,3 +3,8 @@ from typing import List
 
 class DataPayload(BaseModel):
     data: List[str]
+
+class DataJobStatus(BaseModel):
+    job_id: str
+    job_status: str
+    job_progress: float
--- routes/data.py
+++ routes/data.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter, HTTPException
-from models.data_models import DataPayload
+from fastapi import APIRouter, BackgroundTasks
+from models.data_models import DataPayload, DataJobStatus
+import uuid
+import time
 
 router = APIRouter()
 
+processed_results = {}
+KEY_LIST_SIZE = 'list_size'
+KEY_PROCESSED = 'processed_messages'
+
 @router.post("/process-data")
-async def process_data(payload: DataPayload):
+async def process_data(payload: DataPayload, background_tasks: BackgroundTasks) -> str:
     # Access the received data using payload.data
     received_data = payload.data
 
@@ -12,4 +18,42 @@ async def process_data(payload: DataPayload):
     # ...
 
     # Return a response if needed
-    return {"message": "Data received successfully"}
+    result_id = str(uuid.uuid4())
+
+    # Store the identifier and initiate background processing
+    processed_results[result_id] = {
+        KEY_LIST_SIZE:len(received_data),
+        KEY_PROCESSED:[]
+    }
+
+    background_tasks.add_task(process_data_background, payload.data, result_id)
+
+    # Return the result identifier to the client
+    return result_id
+
+@router.get('/get_result/{job_id}')
+async def get_result(job_id: str) -> DataPayload:
+    if job_id not in processed_results:
+        print("Not processed")
+        return DataPayload(data=[])
+    if len(processed_results[job_id][KEY_PROCESSED]) < processed_results[job_id][KEY_LIST_SIZE]:
+        print("Not finished")
+        return DataPayload(data=[])
+    payload = DataPayload(
+        data=processed_results[job_id][KEY_PROCESSED])
+    return payload
+
+@router.get('/job-status/{job_id}')
+async def get_status(job_id: str) -> DataJobStatus:
+    if not job_id in processed_results:
+        return DataJobStatus(job_id=job_id, job_status='Unrecognized', job_progress=0)
+    else:
+        job = processed_results[job_id]
+        progress = len(job[KEY_PROCESSED])/job[KEY_LIST_SIZE]
+        return DataJobStatus(job_id=job_id, job_status='In Progress', job_progress=progress)
+
+
+def process_data_background(data, result_id):
+    for item in data:
+        processed_results[result_id][KEY_PROCESSED].append(item + "_processed")
+        time.sleep(10)
